2a7cb25a8fdff643e4e028716913ec2dd0fa23e2
blackberryfound <kasieapple@hotmail.com>
2023-08-18 18:33:54 +1000

    fixed post recipe

--- flask-server/server.py
+++ flask-server/server.py
@@ -18,7 +18,16 @@ def add_recipe(name, ingredients, method):
 @app.route('/addrecipe', methods=['POST'])
 def api_post_recipe():
     try:
-        add_recipe(request.form.get('name'), request.form.get('ingredients'), request.form.get('method'))
+
+        # data = request.data
+        # recipe = json.dumps(data.decode("utf-8"))
+        # print(recipe)
+        # add_recipe(recipe["name"], recipe["ingredients"], recipe["method"])
+        # add_recipe(request.form.get('name'), request.form.get('ingredients'), request.form.get('method'))
+        data = request.data
+        data = data.decode("utf-8")
+        recipe = json.loads(data)
+        add_recipe(recipe["name"], recipe["ingredients"], recipe["method"])
         return jsonify({'Success': "Yay"})
     except Exception as e:
         return jsonify({'error': str(e)}), 400
@@ -27,13 +36,18 @@ def api_post_recipe():
 def api_get_recipe():
     try:
         recipe_data = recipe_collection.find()
-        return json.loads(json_util.dumps(recipe_data))
+        return json_util.dumps(recipe_data)
     except Exception as e:
         return jsonify({'error': str(e)}), 400
     
 @app.route('/deleterecipe', methods=['POST'])
 def api_post_deleterecipe():
     try:
+        # data = request.data
+        # data = data.decode("utf-8")
+        # recipe = json.loads(data)
+       
+        # recipe_collection.delete_one({'name': recipe["name"]})
         recipe_collection.delete_one({'name': request.form.get('name')})
         return jsonify({'Success': "Yay"})
     except Exception as e:
